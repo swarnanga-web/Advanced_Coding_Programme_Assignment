a36a66b5d7f6f5a072c4460b01da6819a355a614
swarnanga-web <swarnangachakraborty@gmail.com>
2026-05-04 21:04:01 +0530

    adding P1

---
+++ Problem_1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 200005
+
+int main() {
+    char s[MAX];
+    printf("Input: ");
+scanf("%s", s);
+
+    int n = strlen(s);
+
+    // Create circular string
+    char t[2 * MAX];
+    strcpy(t, s);
+    strcat(t, s);
+
+    int freq[26] = {0};
+    int left = 0, max_sum = 0, current_sum = 0;
+
+    for (int right = 0; right < 2 * n; right++) {
+        int idx = t[right] - 'a';
+
+        freq[idx]++;
+        current_sum += (idx + 1);
+
+        // Remove duplicates
+        while (freq[idx] > 1) {
+            int lidx = t[left] - 'a';
+            freq[lidx]--;
+            current_sum -= (lidx + 1);
+            left++;
+        }
+
+        // Ensure window length <= n
+        while (right - left + 1 > n) {
+            int lidx = t[left] - 'a';
+            freq[lidx]--;
+            current_sum -= (lidx + 1);
+            left++;
+        }
+
+        if (current_sum > max_sum) {
+            max_sum = current_sum;
+        }
+    }
+
+    printf("%d\n", max_sum);
+    return 0;
+}
